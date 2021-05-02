41c8a11bfcee3ecc1b4d76afc527ebb227d520a6
Jeffrey Rusterholz <dataforger@gmail.com>
2021-05-02 21:22:03 +0200

    Added static page names fto support code completion.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/pages/home_page.dart';
-import 'package:flutter_template/pages/second_page.dart';
 import 'dart:ui';
 
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+
+import 'package:flutter_template/pages/pages.dart';
+import 'package:flutter_template/pages/home_page.dart';
+import 'package:flutter_template/pages/second_page.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -14,10 +16,6 @@ import 'controllers/theme_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Get and set the locale.
-  Intl.defaultLocale = window.locale.languageCode;
-  await initializeDateFormatting(window.locale.countryCode);
 
   await GetStorage.init();
 
@@ -39,11 +37,17 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark().copyWith(primaryColor: Colors.green),
       themeMode: ThemeController.to.theme.value,
 
+      onReady: () async {
+        // Get and set the locale.
+        Intl.defaultLocale = window.locale.languageCode;
+        await initializeDateFormatting(window.locale.countryCode);
+      },
+
       // Routing.
-      initialRoute: '/',
+      initialRoute: Pages.initialPage,
       getPages: [
-        GetPage(name: '/', page: () => HomePage()),
-        GetPage(name: '/second', page: () => SecondPage()),
+        GetPage(name: Pages.homePage, page: () => HomePage()),
+        GetPage(name: Pages.secondPage, page: () => SecondPage()),
         // Add more pages here.
       ],
       home: HomePage(),
---
+++ lib/pages/pages.dart
@@ -0,0 +1,6 @@
+// Make Page names static to support code completion.
+class Pages {
+  static final initialPage = homePage;
+  static final homePage = '/';
+  static final secondPage = '/second';
+}
